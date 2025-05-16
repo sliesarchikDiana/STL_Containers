1d6f36a0e88147c099b6491db57b68859b3dcd2e
Diana <slesarcukdiana8@gmail.com>
2025-05-16 19:39:46 +0300

    Add clockwise disposition function template

---
+++ prt1_task1.cpp
@@ -0,0 +1,124 @@
+#include "prt1_task1.h"
+#include <iostream>
+
+int main() {
+    const animal cat{"cat", "white", 2};
+    const animal pug{"dog", "brown", 3};
+    const animal fish{"goldfish", "golden", 1};
+    const animal rabbit{"rabbit", "white", 2};
+    const animal parrot{"parakeet", "green", 3};
+
+    std::array<animal, 5> arr = {cat, pug, fish, rabbit, parrot};
+    std::cout << " Array before clockwise shift: ";
+    for (const auto& elem : arr) {
+        std::cout << elem <<" ";
+    }
+    std::cout << std::endl;
+    rotateClockwise(arr, 3);
+
+    std::cout << " Array after clockwise shift: ";
+    for (const auto& elem : arr) {
+        std::cout << elem <<" ";
+    }
+    std::cout << std::endl;
+    std::array<int, 5> int_arr = {1, 2, 3, 4, 5};
+    std::cout << " Array before clockwise shift: ";
+    for (const auto& elem : int_arr) {
+        std::cout << elem <<" ";
+    }
+    std::cout << std::endl;
+    rotateClockwise(int_arr, 2);
+    std::cout << " Array after clockwise shift: ";
+    for (const auto& elem : int_arr) {
+        std::cout << elem <<" ";
+    }
+    std::cout << std::endl<<std::endl;
+
+
+
+
+
+    std::vector<int> numbers1 {1, 2, 3, 4, 5};
+    std::vector<int> numbers2 {10, 20, 30};
+
+    std::cout << "First vector before insertion: ";
+    for (const auto& num : numbers1) {
+        std::cout << num << " ";
+    }
+    std::cout << std::endl;
+    insertRange(numbers1, numbers1.begin() + 3, numbers2);
+
+    std::cout << "First vector after insertion: ";
+    for (const auto& num : numbers1) {
+        std::cout << num << " ";
+    }
+    std::cout << std::endl;
+
+
+    std::vector<animal> animals1 {cat, pug, fish};
+    std::vector<animal> animals2 {rabbit, parrot};
+
+    std::cout << "Animal vector before insertion: ";
+    for (const auto& a : animals1) {
+        std::cout << a << " ";
+    }
+    std::cout << std::endl;
+
+
+    insertRange(animals1, animals1.end(), animals2);
+
+    std::cout << "Animal vector after insertion: ";
+    for (const auto& a : animals1) {
+        std::cout << a << " ";
+    }
+    std::cout << std::endl<<std::endl;
+
+
+
+
+    std::deque<int> dq1 {1, 2, 3, 4, 5};
+
+    std::cout << "Deque before erase (odd amount of elements): ";
+    for (const auto& elem : dq1) {
+        std::cout << elem << " ";
+    }
+    std::cout << std::endl;
+    removeMiddleElements(dq1);
+    std::cout << "Deque after erase: ";
+    for (const auto& elem : dq1) {
+        std::cout << elem << " ";
+    }
+    std::cout <<std::endl;
+
+
+    std::deque<std::string> dq2 {"apple", "banana", "pear", "orange", "tangerine", "plum"};
+
+    std::cout << "Deque before erase (even amount of elements): ";
+    for (const auto& elem : dq2) {
+        std::cout << elem << " ";
+    }
+    std::cout <<std::endl;
+    removeMiddleElements(dq2);
+    std::cout << "Deque after erase: ";
+    for (const auto& elem : dq2) {
+        std::cout << elem << " ";
+    }
+    std::cout << std::endl;
+
+    std::deque<animal> animals {cat, pug, fish, rabbit, parrot};
+    std::cout << "Animal deque before erase: ";
+    for (const auto& a : animals) {
+        std::cout << a << " ";
+    }
+    std::cout << std::endl;
+
+    removeMiddleElements(animals);
+
+    std::cout << "Animal deque after erase: ";
+    for (const auto& a : animals) {
+        std::cout << a << " ";
+    }
+    std::cout << "\n";
+
+    return 0;
+}
---
+++ prt1_task1.h
@@ -0,0 +1,72 @@
+#ifndef PRT1_TASK1_H
+#define PRT1_TASK1_H
+#include <iostream>
+#include <string>
+#include <array>
+#include <utility>
+#include <vector>
+#include <deque>
+
+class animal {
+private:
+    std::string species;
+    std::string color;
+    int age;
+
+public:
+    animal(std::string  species, std::string  color, const int age)
+        : species(std::move(species)), color(std::move(color)), age(age){};
+
+    [[nodiscard]]std::string get_species() const { return species; }
+    [[nodiscard]]std::string get_color() const { return color; }
+    [[nodiscard]]int get_age() const { return age; }
+
+    friend std::ostream& operator<<(std::ostream& os, const animal& a) {
+        os << a.species;
+        return os;
+    }
+
+    ~animal() = default;
+};
+
+template<typename T, std::size_t N> //template for array
+void rotateClockwise(std::array<T, N>& arr, std::size_t shift) {
+    if (N == 0) return; //check whether the array is empty//
+    shift %= N; // a = a %= N (size of array)
+    if (shift == 0) return; // if the array is empty, return//
+    std::vector<T> temp; //creates new temporary array//
+    temp.reserve(N); //pre-allocates space for efficiency.
+
+    for (std::size_t i = 0; i < N; ++i) {
+        temp.push_back(arr[(i + N - shift) % N]); //fills the temporary array with values from the original array wrapping them clockwise//
+    }
+
+    for (std::size_t i = 0; i < N; ++i) {
+        arr[i] = temp[i]; //copies back values to their original array//
+    }
+}
+
+
+
+template<typename T>
+void insertRange(std::vector<T>& destination, typename std::vector<T>::iterator position, const std::vector<T>& source) {
+    destination.insert(position, source.begin(), source.end());
+}
+
+
+
+template<typename T>
+void removeMiddleElements(std::deque<T>& dq) {
+    if (dq.empty()) return;
+    size_t size = dq.size();
+    if (size % 2 == 1) {
+        auto it = dq.begin() + size / 2;
+        dq.erase(it);
+    } else {
+        auto it1 = dq.begin() + (size / 2 - 1);
+        auto it2 = dq.begin() + size / 2 + 1;
+        dq.erase(it1, it2);
+    }
+}
+
+#endif // PRT1_TASK1_H
